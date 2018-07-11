210e8a5df828fcbaf245aafe5ca67147a29a3d5b
Annalisa <arosegraz@gmail.com>
2018-07-10 21:16:18 -0700

    built out menu method in cli.rb

--- lib/know_your_meme/cli.rb
+++ lib/know_your_meme/cli.rb
@@ -4,6 +4,7 @@ def call
   puts "Top 50 Most Popular Memes:"
   list_memes
   menu
+  goodbye
 end
 
 def list_memes
@@ -15,7 +16,30 @@ def list_memes
 end
 
 def menu
-  puts "Enter the number of the meme you'd like more information on:"
+  input = nil
+  while input != "exit"
+    prompt
+    input =  gets.strip.downcase
+    case input
+    when "1"
+      puts "More information on 1."
+    when "2"
+      puts "More information on 2."
+    when "list memes"
+      list_memes
+    else
+      puts "I'm not sure what you want."
+      prompt
+    end
+  end
+end
+
+def prompt
+  puts "Enter the number of the meme on which you'd like more information, or enter 'list memes' to list memes, or enter 'exit' to exit program."
+end
+
+def goodbye
+  "Goodbye."
 end
 
 end
