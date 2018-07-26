61e07a1a478faf14c20554e86f44d2c696c4a256
Annalisa <arosegraz@gmail.com>
2018-07-26 15:39:13 -0700

    scraper is now broken for some reason. starting over...

--- lib/know_your_meme/cli.rb
+++ lib/know_your_meme/cli.rb
@@ -3,42 +3,34 @@ require 'pry'
 class KnowYourMeme::CLI
 
 def call
-  puts "Top 50 Most Popular Memes:"
-  KnowYourMeme::Scraper.scrape_memes
-  start
-end
-
-def start
+  puts "Top 8 Most Popular Memes:"
   list_memes
   prompt
-  print_meme
+  get_input
+  goodbye
 end
 
 def list_memes
+  KnowYourMeme::Scraper.scrape_memes
   memes = KnowYourMeme::Meme.all
+
   memes.each.with_index(1) do |meme, index|
    puts "#{index}. #{meme.name}"
  end
 end
 
-def prompt
-  puts "Enter the number of the meme on which you'd like more information, enter 'list memes' to list memes, or enter 'exit' to exit program."
-end
+def get_input
+    input =  gets.strip.downcase
+    index = input.to_i - 1
 
-def print_meme
-  input =  gets.strip.downcase
-  index = input.to_i - 1
     meme = KnowYourMeme::Meme.all[index]
     KnowYourMeme::Scraper.scrape_meme_details(meme)
-    puts ""
     puts "About"
     puts meme.about
-    puts ""
-    puts "Origin"
-    puts meme.origin
-    puts ""
-    puts "Spread"
-    puts meme.spread
+  end
+
+  def prompt
+    puts "Enter the number of the meme on which you'd like more information, or enter 'list memes' to list memes, or enter 'exit' to exit program."
   end
 
 def exit
--- lib/know_your_meme/scraper.rb
+++ lib/know_your_meme/scraper.rb
@@ -5,7 +5,6 @@ class KnowYourMeme::Scraper
   def self.scrape_memes
     doc = Nokogiri::HTML(open(BASE_URL, 'User-Agent' => 'Chrome'))
 
-    #build doc.css each do loop
     doc.css('.entry-grid-body.infinite td')[0..7].each do |meme_entry|
         name = meme_entry.css('h2 a').text.strip
         url = "https://knowyourmeme.com#{meme_entry.css('a:first-child').attribute('href').value}"
@@ -18,8 +17,6 @@ class KnowYourMeme::Scraper
       doc = Nokogiri::HTML(open(url, 'User-Agent' => 'Chrome'))
 
       meme.about = doc.css('#about + p').text.strip
-      meme.origin = doc.css('#origin + p').text.strip
-      meme.spread = doc.css('#spread + p').text.strip
     end
 
 end
